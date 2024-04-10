19719113b573d10966e0c652722bfd2e1b901f76
leegomes20 <105243028+leegomes20@users.noreply.github.com>
2024-04-10 10:52:48 -0300

    10/04

--- main.js
+++ main.js
@@ -28,7 +28,10 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24)
-    return dias + "dias" - hora + "horas" + minutos + "minutos" + segundos + "segundos";
+    segundos%=60;
+    minutos%=60;
+    horas%24;
+    return [dias + "dias" - hora + "horas" + minutos + "minutos" + segundos + "segundos";]
 
 }
 
